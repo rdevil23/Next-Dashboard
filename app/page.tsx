b2a7756d725250d99669ca01961cc06e23360923
rdevil23 <rdevil1337@gmail.com>
2023-12-07 22:49:09 +0500

    show image on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
@@ -43,7 +42,7 @@ export default function Page() {
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className="hidden md:hidden"
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
